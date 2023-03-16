ac1447ec6b570b7e0c35889a74cf784e78c9c3d8
Frie <fr1e@pm.me>
2023-03-16 13:48:41 +0100

    print url in error messsage

--- check.py
+++ check.py
@@ -10,4 +10,4 @@ r = requests.get(url)
 if r.status_code >= 400: 
     print(r.status_code)
     print(r.headers)
-    raise RuntimeError(f'curl request was not successful. status code: {r.status_code}')
+    raise RuntimeError(f'curl request to {url} was not successful. status code: {r.status_code}')
